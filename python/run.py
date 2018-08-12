78c11cfaafe57734d6261added79abb381ecc1de
Pillow Flat <pillowflat@gmail.com>
2018-08-12 11:27:31 -0400

    config, debugging

--- python/run.py
+++ python/run.py
@@ -11,14 +11,18 @@ API_KEY = os.getenv('API_KEY')
 PLATFORM = os.getenv('PLATFORM')
 NICKNAME = os.getenv('NICKNAME')
 URL = "https://api.fortnitetracker.com/v1/profile/{}/{}".format(PLATFORM, NICKNAME)
+interval = os.getenv('PING_INTERVAL_SEC')
+PING_INTERVAL_SEC = int(interval) if interval.isnumeric() else 5
 
 kills_count = 0
 
 def switch_on():
+    print('Switching on...')
     subprocess.run('wemo switch Outlet on'.split(' '))
 
 def switch_off():
-    subprocess.run('wemo switch Outlet on'.split(' '))
+    print('Switching off...')    
+    subprocess.run('wemo switch Outlet off'.split(' '))
 
 def get_value(key, items):
     for item in items:
@@ -43,6 +47,7 @@ def get_status():
 def execute():
     global kills_count
     status = get_status()
+    pp.pprint(status)
     if (status['kills'] != kills_count):
         kills_count = status['kills']
         switch_on()
@@ -50,9 +55,16 @@ def execute():
         switch_off()
 
 def start():
-    threading.Timer(5.0, start).start()
+    threading.Timer(PING_INTERVAL_SEC, start).start()
     execute()
 
 if __name__ == "__main__":
-    kills_count = get_status()['kills']
+    print('PING_INTERVAL_SEC: {}'.format(PING_INTERVAL_SEC))    
+    print('Testing switch on/off...')
+    switch_on()
+    switch_off()
+    status = get_status()
+    pp.pprint(status)
+    kills_count = status['kills']
     start()
+
